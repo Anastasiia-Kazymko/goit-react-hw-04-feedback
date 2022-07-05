ae41084857c06b4bba8e1081a4f1bb79b1a79056
Anastasiia Kazymko <92369437+AnastasiiaKazymko@users.noreply.github.com>
2022-07-05 18:15:02 +0300

    push step 1

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,16 +1,56 @@
-export const App = () => {
-  return (
-    <div
-      style={{
-        height: '100vh',
-        display: 'flex',
-        justifyContent: 'center',
-        alignItems: 'center',
-        fontSize: 40,
-        color: '#010101'
-      }}
-    >
-      React homework template
-    </div>
-  );
+import React from 'react';
+import {
+  BsFillEmojiFrownFill,
+  BsFillEmojiNeutralFill,
+  BsFillEmojiSmileFill,
+} from 'react-icons/bs';
+
+const icons = {
+  good: BsFillEmojiSmileFill,
+  neutral: BsFillEmojiNeutralFill,
+  bad: BsFillEmojiFrownFill,
 };
+
+export class Feedback extends React.Component {
+  state = {
+    good: 0,
+    neutral: 0,
+    bad: 0,
+  };
+
+  options = Object.keys(this.state);
+
+  ratingIncrement = key => {
+    this.setState(prevState => ({
+      [key]: prevState[key] + 1,
+    }));
+  };
+
+  render() {
+    return (
+      <div>
+        <h1>Please leave feedback</h1>
+        <div>
+          {this.options.map(key => {
+            const Icon = icons[key];
+            return (
+              <button
+                key={key}
+                type="button"
+                onClick={() => this.ratingIncrement(key)}
+              >
+                <Icon /> {key}
+              </button>
+            );
+          })}
+        </div>
+        <h2>Statistics</h2>
+        <div>
+          <p>Good:{this.state.good}</p>
+          <p>Neutral:{this.state.neutral}</p>
+          <p>Bad:{this.state.bad}</p>
+        </div>
+      </div>
+    );
+  }
+}
